55a4fce0218fed9a1fd433bfb910971bc1a32d4f
Constantine SV <alter.blizzard@gmail.com>
2017-05-03 17:19:21 +0300

    add

---
+++ tests/test_Menu.py
@@ -0,0 +1,24 @@
+import unittest
+import Menu
+
+
+class MenuTests(unittest.TestCase):
+    '''
+    def test_game_init(self):
+        a = 12
+        # self.failUnless(game.display)
+        # self.assertRaises(TypeError, game.display, False)
+
+    def test_user_input(self):
+        # i = Menu.process_key()
+        key = "\xe0"
+        i(key)
+        # print(i)
+        # self.failUnless(game.play)
+    '''
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
